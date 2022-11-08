14c9c175c9494a45f1e9a0ab30dbf33625878375
matthew.gaddes <gaddes.matthew@gmail.com>
2022-11-08 14:14:36 +0000

    Working version that saves loss for each batch without averaging. Also add smoothed plotting (rolling average) to make figure easier to interpret.

--- deep_learning_tools/general.py
+++ deep_learning_tools/general.py
@@ -17,47 +17,68 @@ class save_all_metrics(tf.keras.callbacks.Callback):
     Inputs:
         metrics | list of strings | the names of the metrics we're intersted in.  
         batch_metrics | dict | key is metric name, value is a list to store that metric for each epoch.  
-        average | boolean | if True, the tensorflow >=2.2 normal of averaging the batch loss across the epoch is done.  If False, report metrics for that batch
+    Returns:
+        
+    History:
+        2022_11_07 | MEG | Written.  
+        2022_11_08 | MEG | Update to return both averaged and non-averaged loss.  
+
     """
     
-    def __init__(self, metrics, average = False):                                          # metrics is a list of the names of the metrics we want.  Batch metrics is a dict with a list for each metric's value after each batch.  
+    def __init__(self, metrics):                                          # metrics is a list of the names of the metrics we want.  Batch metrics is a dict with a list for each metric's value after each batch.  
     
-        batch_metrics = {}                                                                                                                  # dict to store metrics and their values for every batch
+        batch_metrics_mean = {}                                                                         # dict to store metrics and their values for every batch.  Tensorflow >=2.2 default of averaging each batch over the epoch
+        batch_metrics = {}                                                                              # dict to store metrics and their values for every batch.  No averaging.  
         val_metrics = {}
         for metrics_name in metrics:
+            batch_metrics_mean[metrics_name] = []
             batch_metrics[metrics_name] = []
             val_metrics[f"val_{metrics_name}"] = []
    
         self.metrics = metrics
+        self.batch_metrics_mean = batch_metrics_mean
         self.batch_metrics = batch_metrics
         self.val_metrics = val_metrics
-        self.average = average
-        if average:
-            print(f"\nThe training metrics (loss etc.) that will be saved for each batch will be the tensorflow >= 2.2 onwards normal of the average for all batches that epoch.  \n")
-        else:
-            print(f"\nThe training metrics (loss etc.) that will be saved for each batch will not be the tensorflow >= 2.2 onwards normal of the average for all batches that epoch.  \n")
-        
+    
+    def on_epoch_begin(self, batch, logs=None):                                                                             # 
+    
+        epoch_batch_metrics = {}                                                                # a dict to store the metrics for each batch in each epoch.  
+        for metrics_name in self.metrics:
+            epoch_batch_metrics[metrics_name] = []                                                                  # fill with empty lists.      
+        self.epoch_batch_metrics = epoch_batch_metrics
+    
     
     def on_train_batch_end(self, batch, logs=None):                                                                             # batch is just batch number.  
         
-        for metric in self.metrics:         
-            if batch == 0:                                                                                                     # for the first batch, the batch loss and average batch loss are the same.  
-                self.batch_metrics[metric].append(logs[metric])
+        for metric in self.metrics:                                                                                             # iterate over the metrics we chose (there may be more in logs)
+            self.batch_metrics_mean[metric].append(logs[metric])                                                           # get the mean
+            
+        for metric in self.metrics:                                                                                             # iterate over the metrics we chose (there may be more in logs)    
+            if batch == 0:                                                                                                     # for the first batch...
+                self.epoch_batch_metrics[metric].append(logs[metric])                                                                 # which is equal to the first batch loss, as we only have one batch.  
             else:
-                if self.average == True:                                                                                       # if we're averaging, just take the tensorflow normal (averaged for that epoch) loss
-                    self.batch_metrics[metric].append(logs[metric])
-                else:                                                                                                          # otherwise we need to work out the loss for that batch                     
-                    self.batch_metrics[metric].append(((batch + 1) * logs[metric]) - np.sum(self.batch_metrics[metric]))       # rearranging of average equation to find the last value.  batch starts at 0 so add 1 to make it start at 1
+                batch_loss = ((batch + 1) * logs[metric]) - np.sum(self.epoch_batch_metrics[metric])                                  # rearranging of average equation to find the last value.  batch starts at 0 so add 1 to make it start at 1
+                # if batch_loss < 0:
+                #     pdb.set_trace()
+                self.epoch_batch_metrics[metric].append(batch_loss)
+
                     
-    def on_epoch_end(self, batch, logs=None):                                                         # every epoch, record the validation loss/accuracy
+    def on_epoch_end(self, batch, logs=None):                                                                   # every epoch, record the validation loss/accuracy
         for metric in self.metrics:         
             self.val_metrics[f"val_{metric}"].append(logs[f"val_{metric}"])
+        
+            self.batch_metrics[metric].extend(self.epoch_batch_metrics[metric])
+            
+            
 
 
 #%%
 
 class training_figure_per_epoch(tf.keras.callbacks.Callback):
-        
+    """ Create a figure showing the training up to that epoch.  
+    """
+    
+
     def __init__(self, plotting_function, batch_metrics, epoch_metrics, metrics, title = 'Training metrics',
                  two_column = False, out_path = None, y_epoch_start = 0):
         self.plotting_function = plotting_function
@@ -70,16 +91,24 @@ class training_figure_per_epoch(tf.keras.callbacks.Callback):
         self.y_epoch_start =y_epoch_start
     
     def on_epoch_end(self, epoch, logs={}):                                                                 # overwrite the on_epoch_end default metho in the callback class.  
-        print(f"Plotting the training figure at the end of the epoch")
-        self.plotting_function(self.batch_metrics, self.epoch_metrics, self.metrics,
-                               f"{self.title}_epoch {epoch:02d}", self.two_column, self.out_path, self.y_epoch_start)
+        print(f"Plotting the training figure at the end of epoch {epoch:03d}")
+        if epoch == 0: 
+            y_epoch_start_temp = 0
+        else:
+            y_epoch_start_temp = self.y_epoch_start
+                    
+        self.plotting_function(self.batch_metrics, self.epoch_metrics, self.metrics, f"{self.title} epoch: {epoch:02d}", 
+                               self.two_column, self.out_path / f"model_training_epoch_{epoch:02d}.png", y_epoch_start_temp, no_window = True)                    # noet no_window so that figure is just written to outdiretory.  
+        
+        
             
 
 
 #%%
 
 def plot_all_metrics(batch_metrics, epoch_metrics, metrics = None, title = 'Training metrics', 
-                     two_column = False, out_path = None, y_epoch_start = 0):
+                     two_column = False, out_path = None, y_epoch_start = 0, no_window = False,
+                     n_batch_average = 51):
     """ Given a dict of metrics for every batch and for every epoch, plot the combination of the two.  
     
     Inputs:
@@ -94,12 +123,20 @@ def plot_all_metrics(batch_metrics, epoch_metrics, metrics = None, title = 'Trai
     History:
         2021_11_11 | MEG | Written
         2022_11_01 | MEG | Update so that y limits are adjusted to avoid first epoch values that are very different to rest
+        2022_11_08 | MEG | Add moving average that only plots where valid.  
         
     """
+    import numpy.ma as ma
+    
+    from deep_learning_tools.plotting import moving_average
+
     
     import matplotlib.pyplot as plt    
     import numpy as np
         
+    if no_window:
+        plt.switch_backend('agg')                                                                       # with this backend, no windows are created during figure creation.  
+    
     if metrics is None:                                                                                   # if no specific metrics are requested
         metrics = list(batch_metrics.keys())                                                              # just get them all from the batch metrics
     
@@ -121,18 +158,23 @@ def plot_all_metrics(batch_metrics, epoch_metrics, metrics = None, title = 'Trai
     for plot_n, metric in enumerate(metrics):
         ax = np.ravel(axes)[plot_n]                                                                     # get the ax to plot on
         ax.scatter(xvals_batch, batch_metrics[metric], c = 'k', marker = '.', alpha = 0.5)              # plot for each batch
+        ts_smoothed, valid = moving_average(np.array(batch_metrics[metric]), window = n_batch_average)                   # calculate the moving average, valid is 1 where the full window was available.  
+        ax.plot(ma.array(xvals_batch, mask = (1 - valid)), ma.array(ts_smoothed, mask = (1 - valid)))                    # plot the moving average, but only where valid (rank 1 masked arrays)
         ax.scatter(xvals_epoch, epoch_metrics[f"val_{metric}"], c = 'r', marker = 'o')                           # plot for each epoch
+        ax.plot(xvals_epoch, epoch_metrics[f"val_{metric}"], c = 'r', marker = 'o')                           # plot for each epoch as a line
         ax.set_ylabel(metric)
         ax.grid(True)
 
         if 'accuracy' in metric:                                                                                        # accuracy should increase and can't get higher than 1.  Adjust lower limit
             if (y_epoch_start != 0) or (y_epoch_start != 0.):
-                ax.set_ylim(bottom = batch_metrics[metric][xvals_batch[int(y_epoch_start * n_batches)]], top = 1)      # set y limits, note that upper can be the value after a certain number of epochs (i.e. so can crop out the first high values)
+                #ax.set_ylim(bottom = batch_metrics[metric][xvals_batch[int(y_epoch_start * n_batches)]], top = 1)      # set y limits, note that upper can be the value after a certain number of epochs (i.e. so can crop out the first high values)
+                ax.set_ylim(bottom = ts_smoothed[xvals_batch[int(y_epoch_start * n_batches)]], top = 1)      # set y limits, note that upper can be the value after a certain number of epochs (i.e. so can crop out the first high values)
             else:
                 ax.set_ylim(top = 1)      # set y limits, note that upper can be the value after a certain number of epochs (i.e. so can crop out the first high values)
         else:                                                                                                           # loss should decrease and can't get lower than 0.  Adjust upper limi.t  
             if (y_epoch_start != 0) or (y_epoch_start != 0.):
-                ax.set_ylim(bottom = 0, top = (batch_metrics[metric][xvals_batch[int(y_epoch_start * n_batches)]]))      # set y limits, note that upper can be the value after a certain number of epochs (i.e. so can crop out the first high values)
+                #ax.set_ylim(bottom = 0, top = (batch_metrics[metric][xvals_batch[int(y_epoch_start * n_batches)]]))      # set y limits, note that upper can be the value after a certain number of epochs (i.e. so can crop out the first high values)
+                ax.set_ylim(bottom = 0, top = (ts_smoothed[xvals_batch[int(y_epoch_start * n_batches)]]))      # set y limits, note that upper can be the value after a certain number of epochs (i.e. so can crop out the first high values)
             else:
                 ax.set_ylim(bottom = 0 )      # set y limits, note that upper can be the value after a certain number of epochs (i.e. so can crop out the first high values)
                 
